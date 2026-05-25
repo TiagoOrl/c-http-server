0595726c84bb3446628b3a157055ead28a4ad4e6
Tiago <tiago_orlando@yahoo.com>
2026-05-25 07:50:43 -0300

    refactor: handle request modifies data pointer

--- server/http_handler.h
+++ server/http_handler.h
@@ -10,17 +10,13 @@
 /**
  * handles the request and returns the file data requested in header, returns 0 if not found
  */
-char* handler_handle_request(struct http_req header)
+char* handler_handle_request(struct http_req header, char** res_data)
 {
     const char* res_path_format = "./assets%s";
-    char* res_data = NULL;
     char path[150];
 
     snprintf(path, sizeof(path), res_path_format, header.resource_loc);
-    res_data = file_read(path, "rb");
-
-
-    return res_data;
+    *res_data = file_read(path, "rb");
 }
 
 
@@ -28,7 +24,9 @@ void* server_client_conn_thread(void* arg)
 {
     char in_buffer[1024];
     int* fd = (int*)arg;
+    char response_buffer[1024] = {0};
     struct http_req header;
+    char* resource_data = NULL;
 
     memset(in_buffer, 0, sizeof(in_buffer));
     header.connection = KEEP_ALIVE;
@@ -44,16 +42,20 @@ void* server_client_conn_thread(void* arg)
 
     header = parse(in_buffer, strnlen(in_buffer, 1024));
 
-    char* data = handler_handle_request(header);
+    handler_handle_request(header, &resource_data);
 
-    if (data == NULL)
+    if (resource_data == NULL)
+    {
+        close(*fd);
         return NULL;
+    }
+        
     
 
-    size_t html_len = strnlen(data, MAX_BUFFER_SIZE);
-    char buffer_header[1024] = {0};
+    size_t html_len = strnlen(resource_data, MAX_BUFFER_SIZE);
+    
 
-    int header_len = snprintf(buffer_header, sizeof(buffer_header),
+    int header_len = snprintf(response_buffer, sizeof(response_buffer),
         http_header_res,
         200,
         "OK",
@@ -61,11 +63,11 @@ void* server_client_conn_thread(void* arg)
         html_len
     );
 
-    send(*fd, buffer_header, header_len, 0);
-    send(*fd, data, html_len, 0);
+    send(*fd, response_buffer, header_len, 0);
+    send(*fd, resource_data, html_len, 0);
 
     close(*fd);
-    free(data);
+    free(resource_data);
 
     return NULL;
 }
